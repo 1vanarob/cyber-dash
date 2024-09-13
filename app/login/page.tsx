8ed7572371e57f81178cc583baf8ba853c91a7df
Ivana <21btrcc017@jainuniversity.ac.in>
2024-09-13 19:21:32 +1000

    Added social page template and changed login UI

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,18 +1,24 @@
 import Logo from '@/app/ui/logo';
 import LoginForm from '@/app/ui/login-form';
 import '@/global.css'
- 
+import Image from 'next/image';
 export default function LoginPage() {
   return (
-    <main className="flex items-center justify-center md:h-screen">
-      <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
-        <div className="flex h-20 w-full items-end rounded-lg bg-blue-500 p-3 md:h-36">
-          <div className="w-32 text-white md:w-36">
-            <Logo />
-          </div>
-        </div>
-        <LoginForm />
-      </div>
-    </main>
+
+      <main className="flex items-center justify-center md:h-screen">
+       <div className='shrink w-6/12 h-full border-black bg-blue-500 max-h-fit'>
+       <Image
+          src="/hero-desktop.png"
+        width={1000}
+        height={760}
+        className="hidden md:block"
+        alt="Screenshots of the dashboard project showing desktop version"
+      />
+       </div>
+       <div className="w-6/12 flex-col items-center px-10 rounded-lg">
+          <LoginForm/>
+       </div>
+      </main>
+
   );
 }
