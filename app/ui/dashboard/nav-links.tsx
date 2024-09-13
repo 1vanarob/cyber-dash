33f3af2556839bac39d7e311e350bbc8f07c4b11
Ivana <21btrcc017@jainuniversity.ac.in>
2024-09-13 20:17:36 +1000

    Changes to sidenav

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -3,7 +3,8 @@ import {
   UserGroupIcon,
   HomeIcon,
   ComputerDesktopIcon,
-  ServerIcon
+  ServerIcon,
+  UserIcon
 } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
@@ -19,16 +20,18 @@ const links = [
     icon: ServerIcon,
   },
   { name: 'Technical', href: '/dashboard/technical', icon: ComputerDesktopIcon },
-  {name: 'Social', href: '/dashboard/social',icon:UserGroupIcon}
+  {name: 'Social', href: '/dashboard/social',icon:UserGroupIcon},
+  
 ];
 
 export default function NavLinks() {
   const pathname = usePathname();
   return (
-    <>
+    <main className='flex-col justify-items-between'>
       {links.map((link) => {
         const LinkIcon = link.icon;
         return (
+          
           <Link
             key={link.name}
             href={link.href}
@@ -44,6 +47,22 @@ export default function NavLinks() {
           </Link>
         );
       })}
-    </>
+      <div>
+      <Link 
+      key={"Profile"} 
+      href={"/dashboard/profile"}
+      className={clsx(
+        'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
+        {
+          'bg-sky-100 text-blue-600': pathname === "/dashboard/profile",
+        },
+      )}
+      >
+        <UserIcon className="w-6" />
+        <p className="hidden md:block">Profile</p>
+      </Link>
+      </div>
+     
+    </main>
   );
 }
--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -1,9 +1,11 @@
 import Link from 'next/link';
 import NavLinks from '@/app/ui/dashboard/nav-links';
 import Logo from '@/app/ui/logo';
-import { PowerIcon } from '@heroicons/react/24/outline';
+import { PowerIcon,UserIcon } from '@heroicons/react/24/outline';
 import '@/global.css'
 import { signOut } from '@/auth';
+import clsx from 'clsx';
+import { usePathname } from 'next/navigation';
 
 export default function SideNav() {
   return (
@@ -12,13 +14,14 @@ export default function SideNav() {
         className="mb-2 flex h-20 items-end justify-start rounded-md bg-blue-500 p-4 md:h-40"
         href="/"
       >
-        <div className="w-32 text-white md:w-40">
-          <Logo />
-        </div>
+      <div className="w-32 text-white md:w-40">
+        <Logo />
+      </div>
       </Link>
       <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
         <NavLinks />
         <div className="hidden h-auto w-full grow rounded-md bg-gray-50 md:block"></div>
+        
         <form
           action={async () => {
             'use server';
