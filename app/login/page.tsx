cc093a18319adb122a498cb2b1f0bf91d1fe5a59
Ivana <21btrcc017@jainuniversity.ac.in>
2024-09-19 15:10:46 +1000

    Responsiveness of Login

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,21 +1,22 @@
-import Logo from '@/app/ui/logo';
+
 import LoginForm from '@/app/ui/login-form';
 import '@/global.css'
 import Image from 'next/image';
 export default function LoginPage() {
   return (
-
-      <main className="flex items-center justify-center md:h-screen">
-       <div className='shrink w-6/12 h-full border-black bg-blue-500 max-h-fit'>
+      <main className="flex h-full items-center justify-center sm:h-screen">
+       <div className='hidden p-10 w-fit bg-blue-500 h-full md:block md:w-6/12'>
+       <div className='flex-col items-center p-15 rounded-lg bg-blue-500 h-fit'>
        <Image
           src="/hero-desktop.png"
         width={1000}
         height={760}
-        className="hidden md:block"
+        className="hidden justify-self-center md:block"
         alt="Screenshots of the dashboard project showing desktop version"
       />
+      </div>
        </div>
-       <div className="w-6/12 flex-col items-center px-10 rounded-lg">
+       <div className="w-8/12 flex-col items-center px-15 rounded-lg sm:w-6/12">
           <LoginForm/>
        </div>
       </main>
