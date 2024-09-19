ce6c23ef53f69509a7dfd959b6f4cf9d9208c890
Ivana <21btrcc017@jainuniversity.ac.in>
2024-09-19 22:11:50 +1000

    Center log in text

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -19,13 +19,13 @@ export default function LoginForm() {
   return (
     <form action={formAction} className="space-y-3">
       <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
-        <h1 className={` mb-3 text-[44px]`}>
+        <h1 className={` text-center mb-3 text-[44px]`}>
           Log In
         </h1>
         <div className="w-full">
           <div>
             <label
-              className="mb-3 mt-5 block text-xs font-medium text-gray-900"
+              className="mb-3 mt-5 block text-s font-medium text-gray-900"
               htmlFor="email"
             >
               Email
@@ -44,7 +44,7 @@ export default function LoginForm() {
           </div>
           <div className="mt-4">
             <label
-              className="mb-3 mt-5 block text-xs font-medium text-gray-900"
+              className="mb-3 mt-5 block text-s font-medium text-gray-900"
               htmlFor="password"
             >
               Password
