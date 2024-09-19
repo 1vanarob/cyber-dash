e71f4e39e62bf08ad5b7769761771385dda61053
Ivana <21btrcc017@jainuniversity.ac.in>
2024-09-19 23:35:46 +1000

    HTML changes

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,22 +1,19 @@
-
 import LoginForm from '@/app/ui/login-form';
 import '@/global.css'
 import Image from 'next/image';
 export default function LoginPage() {
   return (
-      <main className="flex h-full items-center justify-center sm:h-screen">
-       <div className='hidden p-10 w-fit bg-blue-500 h-full md:block md:w-6/12'>
-       <div className='flex-col items-center p-15 rounded-lg bg-blue-500 h-fit'>
+      <main className="flex min-h-screen flex-col p-6">
+       <div className='hidden items-justify-center min-h-screen w-fit bg-blue-500 h-screen md:block'>
        <Image
           src="/hero-desktop.png"
         width={1000}
         height={760}
-        className="hidden justify-self-center md:block"
+        className="hidden md:block"
         alt="Screenshots of the dashboard project showing desktop version"
       />
-      </div>
        </div>
-       <div className="w-8/12 flex-col items-center px-15 rounded-lg sm:w-6/12">
+       <div className="w-8/12 flex-col items-center rounded-lg">
           <LoginForm/>
        </div>
       </main>
